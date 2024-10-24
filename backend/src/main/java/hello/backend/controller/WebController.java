4c0c954dbb1f5f1e0f5958e2b0431326516f3fa3
kimmjen <wpals814@gmail.com>
2024-10-24 11:36:23 +0900

    react+spring

---
+++ backend/src/main/java/hello/backend/controller/WebController.java
@@ -0,0 +1,19 @@
+package hello.backend.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController implements ErrorController {
+
+    @GetMapping(value = {
+            "",
+            "/{path:[^.]*}",
+            "/*/{path:[^.]*}",
+            "/*/*/{path:[^.]*}",
+            "/*/*/*/{path:[^.]*}"})
+    public String redirect() {
+        return "forward:/";
+    }
+}
---
+++ front/src/App.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from "axios";
+
+function App() {
+  const [count, setCount] = useState(0)
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => {
+                setMessage(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error!", error);
+            });
+    }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{message}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
